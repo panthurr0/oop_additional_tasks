feab7bcd307fe633459cbf81db488214ce98b3e2
panthurr0 <olapopaola@mail.ru>
2024-01-17 18:43:00 +0300

    №1

--- lesson1/number.py
+++ lesson1/number.py
@@ -15,16 +15,16 @@
 class Number:
 
     def __init__(self, value):
-        ...
+        self.value = value
 
-    def ...(self):
-        ...
+    def get(self):
+        return self.value
 
-    def ...:
-        ...
+    def add(self, value):
+        self.value += value
 
-    def ...:
-        ...
+    def substract(self, value):
+        self.value -= value
 
 
 # код для проверки 
